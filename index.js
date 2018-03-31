534e2dd106c8328d81a50f119eb227c3dc8d0c8a
Helias Fylactos <fylactosdev@gmail.com>
2018-03-31 17:48:18 +0200

    init vue

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+import Vue from 'vue';
+ window.Vue = Vue;
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Welcome to PH'
+  }
+})
+console.log('spiUno');
